d88068829ab7c5d165f6ed3d7a59bde50725c6d8
Pedro Igor <pedrode13souza@gmail.com>
2020-07-28 11:47:10 -0300

    construindo com FutureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,7 @@ import 'dart:convert';
 const request = "https://api.hgbrasil.com/finance";
 
 void main() async {
-
-  runApp(MaterialApp(
-    home: Home()
-  ));
+  runApp(MaterialApp(home: Home()));
 }
 
 Future<Map> getData() async {
@@ -26,13 +23,44 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text("\$ Conversor \$"),
-        backgroundColor: Colors.amber,
-        centerTitle: true,
-      ),
-      body: ,
-    );
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text("\$ Conversor \$"),
+          backgroundColor: Colors.amber,
+          centerTitle: true,
+        ),
+        body: FutureBuilder<Map>(
+            future: getData(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return Center(
+                    child: Text(
+                      "Carregando Dados ...",
+                      style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 25.0,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                default: 
+                  if(snapshot.hasError){
+                    return Center(
+                    child: Text(
+                      "Erro ao carregar",
+                      style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 25.0,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                  }else{
+                    return Container(color: Colors.green,);
+                  }
+              }
+            }));
   }
 }
